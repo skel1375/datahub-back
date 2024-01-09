445ef49a044bd8242a010187d27e60b502227449
skel1375 <chip0923@naver.com>
2024-01-09 16:00:12 +0900

    [Fix] 파일 위치 변경

--- domain/domain-system/src/main/kotlin/com/knusolution/datahub/system/controll/SystemController.kt
+++ domain/domain-system/src/main/kotlin/com/knusolution/datahub/system/controll/SystemController.kt
@@ -1,14 +1,12 @@
 package com.knusolution.datahub.system.controll
 
-import com.knusolution.datahub.system.application.BaseCategoryResponse
-import com.knusolution.datahub.system.application.DetailCategoryResponse
+import com.knusolution.datahub.system.domain.BaseCategoryResponse
+import com.knusolution.datahub.system.domain.DetailCategoryResponse
 import com.knusolution.datahub.system.application.SystemService
-import com.knusolution.datahub.system.domain.SystemEntity
 import com.knusolution.datahub.system.domain.SystemInfo
 import com.knusolution.datahub.system.domain.asDto
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
@@ -18,7 +16,7 @@ class SystemController(
 ) {
     @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
     @GetMapping("/base-category")
-    fun getBaseCategory(@RequestParam systemId:Long):BaseCategoryResponse?
+    fun getBaseCategory(@RequestParam systemId:Long): BaseCategoryResponse?
     {
         if(!systemService.existsDbSystem(systemId)) return null
         val system = systemService.getDbSystem(systemId)
@@ -28,7 +26,7 @@ class SystemController(
 
     @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
     @GetMapping("/detail-category")
-    fun getDetailCategory(@RequestParam baseCategoryId:Long):DetailCategoryResponse?
+    fun getDetailCategory(@RequestParam baseCategoryId:Long): DetailCategoryResponse?
     {
         if(!systemService.existsBaseCategory(id = baseCategoryId)) return null
         val detailCategories = systemService.getDetailCategories(id = baseCategoryId).map{it.asDto()}
